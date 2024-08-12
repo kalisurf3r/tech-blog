e75634e6b9ae78658713f6141ff1147c0c3d5d76
kalisurf3r <ayalaarturo925@gmail.com>
2024-08-11 19:25:04 -0700

    added seeds folder with data

---
+++ seeds/seeds.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { Users, Blogs } = require('../models');
+
+const usersData = require('./usersData.json');
+const blogsData = require('./blogstData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await Users.bulkCreate(usersData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const blog of blogsData) {
+    await Blogs.create({
+      ...blog,
+      userId: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
