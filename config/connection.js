300077fe3431f5ced96d00498f3ff8776db14446
kalisurf3r <ayalaarturo925@gmail.com>
2024-08-19 08:20:42 -0700

    port

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,18 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 let sequelize;
 
-// Use the Render-provided DATABASE_URL if it exists
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true, // Render's PostgreSQL databases require SSL connections
-        rejectUnauthorized: false // This prevents issues with self-signed certificates
-      }
-    }
-  });
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
-  // Fallback for local development
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST || 'localhost',
-      dialect: 'postgres',
+      host: "localhost",
+      dialect: "postgres",
     }
   );
 }
